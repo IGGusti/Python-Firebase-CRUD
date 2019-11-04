6531fc543bfbdfd9606cf0d8fecd830327257c71
Ilham Gumantung Gusti <iggusti@gmail.com>
2019-11-04 09:28:28 +0700

    add Create

---
+++ Create.py
@@ -0,0 +1,11 @@
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication("https://python-firebase-63d55.firebaseio.com/", None)
+data = {
+    'Nama':'Ilham Gumantung Gusti',
+    'Email':'iggusti@gmail.com',
+    'HP':82126258198
+}
+
+result = firebase.post('/Customer', data)
+print(result)
